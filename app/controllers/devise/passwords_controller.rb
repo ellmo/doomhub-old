5f914f47b496d10420224df50a08c1b8c965bb1e
ellmo <ellmunzai@gmail.com>
2012-02-28 04:20:53 -0800

    passwords controller fix

--- app/controllers/devise/passwords_controller.rb
+++ app/controllers/devise/passwords_controller.rb
@@ -12,7 +12,7 @@ class Devise::PasswordsController < ApplicationController
   def create
     self.resource = resource_class.send_reset_password_instructions(params[resource_name])
 
-    if successful_and_sane?(resource)
+    if successfuly_sent?(resource)
       set_flash_message(:notice, :send_instructions) if is_navigational_format?
       respond_with({}, :location => after_sending_reset_password_instructions_path_for(resource_name))
     else
