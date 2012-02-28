4cd8469b955adb1363a2013b5437049f19b9966c
ellmo <ellmunzai@gmail.com>
2012-02-28 02:13:19 +0100

    production mailer setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,17 @@ Doomhub::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'doomhub.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => 'gmail.com',
+      :user_name            => 'doomhub@gmail.com',
+      :password             => ENV['DOOMHUB_GMAIL_PASS'],
+      :authentication       => 'plain',
+      :enable_starttls_auto => true
+  }
 
   # Enable threaded mode
   # config.threadsafe!
