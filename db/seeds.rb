616ff1ca1a57e090d72f2b003e7bb3a5ad2a7ad1
ellmo <ellmunzai@gmail.com>
2012-03-31 17:27:04 +0200

    Not what I had in mind. Not for now, at least.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Emanuel', :city => cities.first)
 Game.create(:name => "Doom", :default_lumpname => "E1M1")
 Game.create(:name => "Doom 2", :default_lumpname => "MAP01")
 Game.create(:name => "Heretic", :default_lumpname => "E1M1")
