55c833fe76c5eb91742c6da77cba2773e6c3cabd
ellmo <ellmunzai@gmail.com>
2012-06-23 23:45:09 +0200

    added 403 static error page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery
 
-  add_breadcrumb "DoomHub.com", :root_path
+  rescue_from CanCan::AccessDenied do |exception|
+    render :file => "#{Rails.root.to_s}/public/403.haml", :status => 403, :layout => false
+  end
+
+  add_breadcrumb "Doomhub.com", :root_path
 
   def superadmin?
     current_user.superadmin?
