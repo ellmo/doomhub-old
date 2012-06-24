89183a93e946929c15a9e6ffcd07238541a88705
ellmo <ellmunzai@gmail.com>
2012-06-24 02:23:43 +0200

    Jesus, Devise is going to be the end of me ...some day

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,8 @@ Doomhub::Application.routes.draw do
   devise_for :user, :path => '', :path_names => {
     :sign_in => "login",
     :sign_out => "logout",
-    :sign_up => "register"
+    :sign_up => "new",
+    :registration => 'registration'
   }
 
   get "home/index"
