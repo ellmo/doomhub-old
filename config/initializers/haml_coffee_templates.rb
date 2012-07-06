e1bb65be3708c63010b911fcc277265f1ebe8cb7
ellmo <ellmunzai@gmail.com>
2012-07-06 20:48:00 +0200

    deleted obsolete (?) initializer for HAML Coffee Templates

--- config/initializers/haml_coffee_templates.rb
+++
@@ -1,3 +0,0 @@
-### Seems like it's not needed in Rails 3.2, but I'll leave it just in case for one more commit
-
-#HamlCoffeeAssets::HamlCoffeeTemplate.name_filter = lambda { |n| n.sub /^backbone\/templates\//, '' }
