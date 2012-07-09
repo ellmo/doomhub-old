69ea19e30586ee767f76a6a5d557f42e4adeea14
ellmo <ellmunzai@gmail.com>
2012-07-09 02:48:46 +0200

    foundation3 styles for toolbar and breadcrumbs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,14 @@ module ApplicationHelper
     data_hash
   end
 
+  def nav_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'active' : ''
+
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
+  end
+
   def user_login_span_tag(user = nil)
     if user
       str = <<-HTML
