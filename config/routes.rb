2fee42fd43bc5bfacd3ac3908777ee3e5b6c1b78
ellmo <ellmunzai@gmail.com>
2012-08-17 21:29:38 +0200

    simplyfying routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,18 @@ Doomhub::Application.routes.draw do
 
   root :to => 'home#index'
 
-  get "/projects/:id/maps" => redirect("/projects/%{id}#maps")
-  get "/projects/:project_id/maps/:id/wadfiles" => redirect("/projects/%{project_id}/maps/%{id}#wadfiles")
+  get "/p/:id/m" => redirect("/p/%{id}#maps")
+  get "/p/:project_id/m/:id/w" => redirect("/p/%{project_id}/m/%{id}#wadfiles")
+  get "/p/:project_id/m/:id/i" => redirect("/p/%{project_id}/m/%{id}#images")
 
-  resources :projects do
-    resources :maps, :except => [:index] do
-      resources :map_wadfiles, :except => [:index] do
+  resources :projects, :path => '/p' do
+    resources :maps, :path => '/m', :except => [:index] do
+      resources :map_wadfiles, :path => '/w', :except => [:index] do
         member do
           get :download
         end
       end
-      resources :map_images, :except => [:index] do
+      resources :map_images, :path => '/i', :except => [:index] do
         member do
           get :auth_url
         end
@@ -20,8 +21,8 @@ Doomhub::Application.routes.draw do
     end
   end
 
-  resources :users, :only => [:show, :index] do
-    resources :projects
+  resources :users, :path => '/u', :only => [:show, :index] do
+    resources :projects, :path => '/p'
   end
 
   devise_for :user, :path => '', :path_names => {
