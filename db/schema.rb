9c353586469462a82a5c8ffecaaaeca59c201f9d
ellmo <ellmunzai@gmail.com>
2013-10-29 17:44:07 +0100

    fixing create_users migration to match devise`s pg migration

--- db/schema.rb
+++ db/schema.rb
@@ -153,22 +153,22 @@ ActiveRecord::Schema.define(:version => 20131025095805) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "",    :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "",    :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
+    t.integer  "sign_in_count",          :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                               :null => false
-    t.datetime "updated_at",                                               :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
     t.string   "login"
-    t.integer  "user_role_id",                          :default => 3
+    t.integer  "user_role_id",           :default => 3
     t.datetime "deleted_at"
-    t.boolean  "banned",                                :default => false, :null => false
+    t.boolean  "banned",                 :default => false, :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
