4b428e8373a04fdcf32533854c15c5dbef7fe844
ellmo <ellmunzai@gmail.com>
2013-11-21 01:42:39 +0100

    all specs fixed(?) [#25]

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,6 +4,8 @@ class Author < ActiveRecord::Base
 #= ASSOC
 #======
 
-  has_many :maps, :as => :author
+  has_many :maps, :as => :authorable
+  has_many :map_wadfiles, as: :authorable
+  has_many :map_images, as: :authorable
 
 end
--- app/models/file_link.rb
+++ app/models/file_link.rb
@@ -2,5 +2,5 @@ class FileLink < ActiveRecord::Base
   attr_accessible :url
 
   belongs_to :file_linkable, polymorphic: true
-  belongs_to :author, polymorphic: true
+  belongs_to :authorable, polymorphic: true
 end
