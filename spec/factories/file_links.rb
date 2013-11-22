583d94eb8472caa354866956f7ea3fcc10b89762
ellmo <ellmunzai@gmail.com>
2013-11-22 20:41:06 +0100

    more comfortable with this factory notation [#2]

--- spec/factories/file_links.rb
+++ spec/factories/file_links.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory :file_link do
-    association :authorable, factory: :user
-    # authorable { create :user }
+    authorable { create :user }
     file_linkable {|fl| FactoryGirl.create(:project, creator: fl.authorable) }
     url 'http://www.mediafire.com/download/1234567890/example.zip'
   end
