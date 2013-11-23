dcf9b790bcb9c4381b1b62060ad94b4f34169775
ellmo <ellmunzai@gmail.com>
2013-11-23 20:30:00 +0100

    FileLink model url validation [#2]

--- app/models/file_link.rb
+++ app/models/file_link.rb
@@ -3,4 +3,12 @@ class FileLink < ActiveRecord::Base
 
   belongs_to :file_linkable, polymorphic: true
   belongs_to :authorable, polymorphic: true
+
+  validate :http_url
+
+  def http_url
+    errors[:url] << 'not a valid url' unless URI.parse(url).kind_of?(URI::HTTP)
+  rescue URI::InvalidURIError
+    errors[:url] << 'not a valid url'
+  end
 end
