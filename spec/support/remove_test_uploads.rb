66233b5102035dc89a2fa8d203e25c55cbc78ea2
ellmo <ellmunzai@gmail.com>
2014-01-08 20:42:39 +0100

    moved test upload path to RAILS_ROOT/public/upload

--- spec/support/remove_test_uploads.rb
+++ spec/support/remove_test_uploads.rb
@@ -1,5 +1,5 @@
 def remove_test_uploads
-  dir = File.join(Rails.root, 'uploads', 'test')
+  dir = File.join(Rails.root, 'public', 'uploads', 'test')
   if File.directory? dir
     FileUtils.rm_rf dir
   end
