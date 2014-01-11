ba72cc0962178160961dba6f9fc54bd1ec47ad22
ellmo <ellmunzai@gmail.com>
2014-01-11 16:34:03 +0100

    [#21]

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,7 +4,7 @@ class Author < ActiveRecord::Base
 #= ASSOC
 #======
 
-  has_many :maps, :as => :authorable
+  has_many :maps, as: :authorable
   has_many :map_wadfiles, as: :authorable
   has_many :map_images, as: :authorable
   has_many :file_links, as: :authorable
