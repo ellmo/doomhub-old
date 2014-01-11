0b133232ea9736d0ab42fbc3196dadddee2094c0
ellmo <ellmunzai@gmail.com>
2014-01-11 16:29:16 +0100

    adding comments to FileLink model [#2]

--- app/models/file_link.rb
+++ app/models/file_link.rb
@@ -1,10 +1,27 @@
 class FileLink < ActiveRecord::Base
-  attr_accessible :url
+
+#========
+#= ASSOC
+#======
 
   belongs_to :file_linkable, polymorphic: true
   belongs_to :authorable, polymorphic: true
 
+#=======
+#= ATTR
+#=====
+
+  attr_accessible :url
+
+#============
+#= CALLBACKS
+#==========
+
   validate :http_url
+
+#============
+#= METHODS
+#==========
 
   def http_url
     errors[:url] << 'not a valid url' unless URI.parse(url).kind_of?(URI::HTTP)
